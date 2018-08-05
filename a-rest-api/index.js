3d41cdec86ccef8f9c1b23468182d01dd49a700a
cverdi <cristina.verdi.tobon@gmail.com>
2018-08-06 00:22:37 +0200

    refactor: extract 'success on server' and 'request details' logging functions

--- a-rest-api/index.js
+++ a-rest-api/index.js
@@ -7,25 +7,28 @@ const host = 'localhost';
 const server = http.createServer((request, response) => {    const parsedUrl = url.parse(request.url, true);
     const trimmedPath = parseAndTrimmPath(request);
     const method = parseHttpMethod(request);
-    
+    const query = parseQueryString(request);
+
+    logRequestDetails(trimmedPath, method, query);
+
     sendResponse(response);
-
-    console.log(`request received on path ${ trimmedPath } with method ${ method }`)
 });
 
-server.listen(port, host, () => {
-    console.log(`server up and running on port ${ port }`);
-});
+server.listen(port, host, () => logSuccessMessage());
 
 const parseHttpMethod = (request) => request.method;
-
 const parseAndTrimmPath = (request) => {
-    const parsedUrl = url.parse(request.url, true);
+    const parsedUrl = parseUrl(request);
     return trimmPath(parsedUrl.pathname);
 }
-
+const parseQueryString = (request) => parseUrl(request).query;
+const parseUrl = (request) => url.parse(request.url, true);
 const trimmPath = (path) => path.replace(/^\/+|\/$/g, '');
-
-const sendResponse = (response) => {
-    response.end('Hello World\n');
+const sendResponse = (response) =>  response.end('Hello World\n');
+const logSuccessMessage = () => console.log(`server up and running on port ${port}`);
+const logRequestDetails = (path, method, query) => {
+    console.log(`request received on ->\n
+    Path: ${ path }\n 
+    Method: ${ method }\n
+    Query: ${ JSON.stringify(query) }\n`);
 }
